f4084ae725e5b1b3931a5a4047ef11fb810e0fbf
E_yo <dbscks2140@gmail.com>
2023-06-14 16:31:40 +0900

    Carousel

---
+++ Carousel/review.js
@@ -0,0 +1,35 @@
+const reviewInfo = [
+  {
+    name: 'chany',
+    review: 'Such a cool photo! Looks great printed out and framed!!',
+    img:
+      'https://i.etsystatic.com/37365047/r/il/2bc493/4743364332/il_794xN.4743364332_qowb.jpg',
+  },
+
+  {
+    name: 'Tomy',
+    review: 'Looks so dope as part of my gallery wall! Love it!',
+    img:
+      'https://i.etsystatic.com/37365047/r/il/67d399/4688449880/il_794xN.4688449880_bboo.jpg',
+  },
+  {
+    name: 'Yamy',
+    review: 'Love it!!! Perfect. Came out gorgeous in a gold ornate frame. Thank you!',
+    img:
+      'https://i.etsystatic.com/37365047/r/il/1aeb1e/4736634057/il_794xN.4736634057_9y4g.jpg',
+  },
+  {
+    name: 'Sony',
+    review: 'I love this print! Looks perfect in my bathroom. Thank you!!',
+    img:
+      'https://i.etsystatic.com/37365047/r/il/7e0016/4743457560/il_794xN.4743457560_mum3.jpg',
+  },
+  {
+    name: 'Canon',
+    review: 'Fantastic photo of history that must be kept alive!!',
+    img:
+      'https://i.etsystatic.com/37365047/r/il/cac276/4766052847/il_794xN.4766052847_sw9h.jpg',
+  }
+];
+
+export default reviewInfo;

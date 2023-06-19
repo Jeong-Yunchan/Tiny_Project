cb296964340cc7c83fe0de0f12b153aeec8b9d81
E_yo <dbscks2140@gmail.com>
2023-06-19 20:40:26 +0900

    Side Bar 수정

--- Side Bar/App.js
+++ Side Bar/App.js
@@ -4,7 +4,6 @@ const side_list_back = document.querySelector('.fa-arrow-right');
 
 aside.addEventListener('click',()=>{
   side_list.classList.toggle('visible-true');
-  side_list_back.classList.toggle('visible-true');
   aside.style.display = 'none';
   side_list_back.style.display = 'block';
 });
@@ -14,3 +13,7 @@ side_list_back.addEventListener('click',()=>{
   aside.style.display = 'block';
   side_list_back.style.display = 'none';
 });
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
